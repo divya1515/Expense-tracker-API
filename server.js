17bd405802d5957e7ad7e08d55a647352ef4ac51
divya747 <divya.kaushik@pibit.ai>
2025-07-02 10:55:08 +0530

    add minor change

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ const PORT=process.env.PORT||5000;
 
         // await sequelize.sync({force:true});//all the tables mention in model are created 
         await sequelize.sync();
-        app.listen(PORT,()=>{
-            console.log(`Server running on  http://localhost:${PORT}`);
+        //0.0.0.0 means it will accept request from all interfaces not only localhost ...as in docker then if localhost then it will not listen outside docker if mentioned localhost then it will not listen outside docker if mentioned 0.0.0.0 then it will listen outside docker
+        app.listen(PORT,'0.0.0.0',()=>{ 
+            console.log(`Server running on  http://0.0.0.0:${PORT}`);
         })
 
 
